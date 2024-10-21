942f8b06a81e4ab3290f4b03234dbaf20b5846c2
Kobe Swier <kobe.swier@southeasttech.edu>
2024-10-20 20:56:40 -0500

    10/20

---
+++ Unit 2 Project/Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Unit_2_Project.Models;
+
+namespace Unit_2_Project.Controllers
+{
+    // Controller to manage actions
+    public class EmployeesController : Controller
+    {
+        // Injection for DbContext
+        private EmployeeDBContext context { get; set; }
+
+        // Accepts DbContext
+        public EmployeesController(EmployeeDBContext context)
+        {
+            this.context = context;
+        }
+
+        // Retrievs list of employees from database
+        public IActionResult Index()
+        {
+            var employees = context.Employees.OrderBy(c => c.Id).ToList();
+            return View(employees);
+        }
+    }
+}
