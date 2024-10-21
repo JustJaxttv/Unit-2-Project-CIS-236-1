ae87cce5c81c1da7e4b0eb95ad6c6f51fdff0f33
Kobe Swier <kobe.swier@southeasttech.edu>
2024-10-20 21:10:40 -0500

    10/20

---
+++ Unit 2 Project/Migrations/20241021020631_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unit_2_Project.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Name", "Position", "Salary" },
+                values: new object[,]
+                {
+                    { 1, "Berg, David", "Manager", 75000m },
+                    { 2, "Smith, Arnold", "Salesman", 50000m },
+                    { 3, "Reham, Amanda", "Supervisor", 62000m },
+                    { 4, "Tylar, Darrin", "Salesman", 48000m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
