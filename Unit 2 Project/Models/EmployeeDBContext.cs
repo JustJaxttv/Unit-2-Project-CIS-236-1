e7b7e64b3911a35b2c26f0a189d4bdd7a1d90a8f
Kobe Swier <kobe.swier@southeasttech.edu>
2024-10-20 20:14:17 -0500

    Add project files.

---
+++ Unit 2 Project/Models/EmployeeDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Unit_2_Project.Models
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee() { Id = 1, Name = "Berg, David", Position = "Manager" },
+                new Employee() { Id = 2, Name = "Smith, Arnold", Position = "Salesman" }
+            );
+        }
+    }
+}
