c57c2a8ed12f023a5e4e94dc0094dd0a0e45b3cc
Kobe Swier <kobe.swier@southeasttech.edu>
2024-10-20 20:26:58 -0500

    10/20

--- Unit 2 Project/Models/Employee.cs
+++ Unit 2 Project/Models/Employee.cs
@@ -1,9 +1,18 @@
 ﻿namespace Unit_2_Project.Models
 {
+    // Employee model that represents an Employee from the database
     public class Employee
     {
+        // Primary Key
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Position { get; set; }
+
+        // Employee Name
+        public string? Name { get; set; }
+
+        // Employee Job Title
+        public string? Position { get; set; }
+
+        // Employee Salary
+        public decimal? Salary { get; set; }
     }
 }
--- Unit 2 Project/Models/EmployeeDBContext.cs
+++ Unit 2 Project/Models/EmployeeDBContext.cs
@@ -2,17 +2,24 @@
 
 namespace Unit_2_Project.Models
 {
+    // DbContext class for database session and mapping of employee entities
     public class EmployeeDBContext : DbContext
     {
+        // Contructor that passes options to base class
         public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options) { }
 
+        // Represents the Employees table
         public DbSet<Employee> Employees { get; set; }
 
+        // Initial data for Employees table
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Employee>().HasData(
-                new Employee() { Id = 1, Name = "Berg, David", Position = "Manager" },
-                new Employee() { Id = 2, Name = "Smith, Arnold", Position = "Salesman" }
+                new Employee() { Id = 1, Name = "Berg, David", Position = "Manager", Salary = 75000 },
+                new Employee() { Id = 2, Name = "Smith, Arnold", Position = "Salesman", Salary = 50000 },
+                new Employee() { Id = 3, Name = "Reham, Amanda", Position = "Supervisor", Salary = 62000 },
+                new Employee() { Id = 4, Name = "Tylar, Darrin", Position = "Salesman", Salary = 48000 }
             );
         }
     }
